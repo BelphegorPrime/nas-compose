fc946c45fd2cd22bba54b8b8c8cf1e61f98222f3
Marcel Rösler <roesler1@gmx.de>
2021-03-04 21:29:28 +0100

    fix

--- scripts/cold/cold8TB.sh
+++ scripts/cold/cold8TB.sh
@@ -2,7 +2,7 @@
 
 # 28b81fe9-324f-4956-9b99-9136a371f7db === cold8TB (7,22 TB)
 # Size ~ 2,99TB
-rsync -avu --delete /srv/dev-disk-by-label-media/media/Anime /srv/dev-disk-by-uuid-0269d75b-4f9f-44bc-9143-b81ae1b547df/
+rsync -avu --delete /srv/dev-disk-by-label-media/media/Anime /srv/dev-disk-by-uuid-28b81fe9-324f-4956-9b99-9136a371f7db/
 # Size ~ 6,4TB
 rsync -avu --delete /srv/dev-disk-by-label-media/media/Games /srv/dev-disk-by-uuid-28b81fe9-324f-4956-9b99-9136a371f7db/
 # Sizes Total ~ 6,4TB
