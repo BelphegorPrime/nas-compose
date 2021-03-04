b9e2b6c4ec1301eed42326b6bd00438f7d6eb9fa
Marcel Rösler <roesler1@gmx.de>
2021-03-04 21:45:21 +0100

    more refactor

---
+++ scripts/cold/cold16TB.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# xxxxxxxxxxxx === cold16TB (7,22 TB)
+# Size > 7TB
+#rsync -av --delete /srv/dev-disk-by-label-media/media/Series /srv/dev-disk-by-uuid-xxxxxxxxxxxx/
+
+# Size ~ 0,000TB
+#rsync -avu --delete /srv/dev-disk-by-label-media/media/Movies /srv/dev-disk-by-uuid-xxxxxxxxxxxx/
+# Sizes Total > 7TB
--- scripts/runBackupColdStorage.sh
+++ scripts/runBackupColdStorage.sh
@@ -1,9 +1,7 @@
 #! /usr/bin/env bash
 
 bash cold/cold2TB.sh;
+
 bash cold/cold8TB.sh;
 
-# Size > 7TB
-#rsync -av --delete /srv/dev-disk-by-label-media/media/Series /srv/dev-disk-by-uuid-xxxxxxxxxxxx/
-# Size ~ 0,000TB
-#rsync -avu --delete /srv/dev-disk-by-label-media/media/Movies /srv/dev-disk-by-uuid-28b81fe9-324f-4956-9b99-9136a371f7db/
+bash cold/cold16TB.sh;
