ac89b3d1ff1d66e5c4b7273578c58db874403ffb
Marcel Rösler <roesler1@gmx.de>
2021-03-06 21:55:16 +0100

    update sizes

--- scripts/cold/cold16TB.sh
+++ scripts/cold/cold16TB.sh
@@ -5,12 +5,12 @@ DIR="/srv/dev-disk-by-label-cold16TB"
 # cold16TB (14,55 TB)
 if [[ -d $DIR ]]
 then
-  # Size > 7TB
+  # Size ~ 7,1TB
   rsync -avu --delete /srv/dev-disk-by-label-media/media/Series $DIR/
 
-  # Size ~ 0,000TB
+  # Size ~ 0,344TB
   rsync -avu --delete /srv/dev-disk-by-label-media/media/Movies $DIR/
-  # Sizes Total > 7TB
+  # Sizes Total ~ 7,444TB
 else
   echo "cold16TB drives directory $DIR does not exist"
 fi
