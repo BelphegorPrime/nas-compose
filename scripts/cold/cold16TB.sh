1ce351f51d40004fba06d70e7df1c74dd1d1f945
Marcel Rösler <roesler1@gmx.de>
2021-03-06 13:03:27 +0100

    add new cold drive

--- scripts/cold/cold16TB.sh
+++ scripts/cold/cold16TB.sh
@@ -1,17 +1,15 @@
 #! /usr/bin/env bash
 
-UUID=xxxxxxxxxxxx
-DIR="/srv/dev-disk-by-uuid-$UUID"
+DIR="/srv/dev-disk-by-label-cold16TB"
 
+# cold16TB (14,55 TB)
 if [[ -d $DIR ]]
 then
-  echo $DIR
-  # xxxxxxxxxxxx === cold16TB (7,22 TB)
   # Size > 7TB
-  #rsync -av --delete /srv/dev-disk-by-label-media/media/Series $DIR/
+  rsync -avu --delete /srv/dev-disk-by-label-media/media/Series $DIR/
 
   # Size ~ 0,000TB
-  #rsync -avu --delete /srv/dev-disk-by-label-media/media/Movies $DIR/
+  rsync -avu --delete /srv/dev-disk-by-label-media/media/Movies $DIR/
   # Sizes Total > 7TB
 else
   echo "cold16TB drives directory $DIR does not exist"
