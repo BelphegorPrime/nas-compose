0e390ba46383310e2f989fc042f1fee9fb70b19a
root <root@localhost.localdomain>
2022-07-16 17:00:00 +0200

    fix APU null pointer exception

--- scripts/cold/cold16TB.sh
+++ scripts/cold/cold16TB.sh
@@ -2,7 +2,7 @@
 
 #DIR=""
 #DIR="/srv/dev-disk-by-id-usb-TOSHIBA_MG08ACA16TE_RANDOM__42CC1D61B6A0-0-1-part1"
-DIR="/srv/dev-disk-by-id-usb-TOSHIBA_MG08ACA16TE_RANDOM__DA02D6BA993A-0-1-part1"
+DIR="/srv/dev-disk-by-uuid-90b9d203-d281-4e8e-aec9-ecaae020703f"
 
 # cold16TB (14,55 TB)
 if [[ -d $DIR ]]
